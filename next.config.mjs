f67cf53c701374f838ea447668be0130653cc0ff
BestTTTA <bst12546@gmail.com>
2025-06-23 01:11:25 +0700

    Add allow domain image url

--- next.config.mjs
+++ next.config.mjs
@@ -9,8 +9,19 @@ const nextConfig = {
         pathname: '/**',
       },
       {
+        protocol: 'http',
+        hostname: '119.59.99.192',
+        port: '8000',
+        pathname: '/**',
+      },
+      {
         protocol: 'https',
         hostname: 'facefindr.api.thetigerteamacademy.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'be-facefindr.thetigerteamacademy.net',
         pathname: '/**',
       },
     ],
